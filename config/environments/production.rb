c4216ea66d4e576f0563978364ca44569de2b167
thkernel <a.dembele@auctum.ml>
2019-07-20 09:46:42 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #For mail
+=begin
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
@@ -107,5 +108,16 @@ Rails.application.configure do
     enable_starttls_auto: true,
     :ssl => true
   }
+=end 
+config.action_mailer.default_url_options = { host: 'http://flirtera.net' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'flirtera.official@gmail.com',
+  password:             'AMOSXZIBITDE88',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 
 end
