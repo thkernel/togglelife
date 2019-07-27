e711e4d60ba94d61d4efc5044b34eca5bd482c26
thkernel <a.dembele@auctum.ml>
2019-07-27 17:32:08 +0000

    update

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbolinks:load', function(event) {
     if (typeof gtag === 'function') {
-      gtag('config', 'UA-139212540-2', {
+      gtag('config', 'UA-144729623-1', {
         'page_location': event.data.url
       });
     }
