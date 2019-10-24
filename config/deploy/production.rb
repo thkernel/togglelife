f19373ee11a251205cc3cfef7ba4d91b38c1451f
thkernel <a.dembele@auctum.ml>
2019-10-24 17:44:43 +0000

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,7 +10,7 @@ set :application, "flirtera.net"               # the name of your website - shou
 set :deploy_to, "/home/ubuntu/rails-apps/flirtera.prod"   # the path to your new deployment directory on the server - by default, the name of the application (e.g. "/var/www/sites/example.com")
 set :rails_env, "production"
 
-server "18.217.182.48", user: "ubuntu", roles: %w{app db web}, ssh_options: {forward_agent: true}
+server "51.38.235.131", user: "root", roles: %w{app db web}, ssh_options: {forward_agent: true}
 
 set :branch, "master"
 
