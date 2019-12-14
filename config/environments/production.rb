baeb979dccd9b468bd98280c2eff453287e47c4b
thkernel <a.dembele@auctum.ml>
2019-12-14 10:18:50 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,9 +3,9 @@ Rails.application.configure do
 
   #force HTTPS on production
   #config.force_ssl = true
-  config.to_prepare { Devise::SessionsController.force_ssl }
-  config.to_prepare { Devise::RegistrationsController.force_ssl }
-  config.to_prepare { Devise::PasswordsController.force_ssl }
+  #config.to_prepare { Devise::SessionsController.force_ssl }
+  #config.to_prepare { Devise::RegistrationsController.force_ssl }
+  #config.to_prepare { Devise::PasswordsController.force_ssl }
   
   # Code is not reloaded between requests.
   config.cache_classes = true
